cf0eea63c2a6aac1e41f9dc4ea1261f9c2f690bc
Veronica O <veronica.o.garcia@gmail.com>
2021-05-20 02:03:23 -0500

    update api

--- api_keys2.py
+++
@@ -1,9 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "75aadc30a7910e5bb5da8ab4fa73ca42"
-
-# Google API Key
-g_key = "AIzaSyAoFVwpqhDg20leYOOUInRpB5rE5JwGNjE"
-
-
-url = "http://api.openweathermap.org/data/2.5/weather?"
-units = "metric"
